a4c7a5af1740ba08e1b71027bcb6a45ee541fcf1
Kristian Hakala <khakala@c3r4p6.hive.fi>
2020-09-01 16:34:07 +0300

    Added collision and added different coloring for walls depending on direction and hitpoint of the ray

--- srcs/collision.c
+++ srcs/collision.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   collision.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsjoberg <lsjoberg@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: khakala <khakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 16:59:23 by lsjoberg          #+#    #+#             */
-/*   Updated: 2020/08/26 15:50:07 by lsjoberg         ###   ########.fr       */
+/*   Updated: 2020/08/31 15:16:02 by khakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@ void	set_collision(t_w3d *w)
 
 	x = (int)(w->cam.moveX);
 	y = (int)(w->cam.moveY);
-	if ((w->cam.moveX > 0 && w->cam.moveX < 25) \
-		&& (w->cam.moveY > 0 && w->cam.moveY < 25))
+	/*if ((w->cam.moveX > 0 && w->cam.moveX < w->grid.height - 1) \
+		&& (w->cam.moveY > 0 && w->cam.moveY < w->grid.width - 1))
+	{
+		w->cam.posX = w->cam.moveX;
+		w->cam.posY = w->cam.moveY;
+	}*/
+	if ((w->grid.matrix[x][y]) == 0)
 	{
 		w->cam.posX = w->cam.moveX;
 		w->cam.posY = w->cam.moveY;
